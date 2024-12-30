fdf2b82631e11ef58ec9bf3b8e3a2465fdb04480
raulntjj <raulntjj@gmail.com>
2024-12-30 19:21:26 -0300

    Adding basic structure to operate multi tenancy module

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
-{
+class User extends Model implements AuthenticatableContract, AuthorizableContract {
     use Authenticatable, Authorizable, HasFactory;
 
     /**
@@ -19,7 +18,10 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string[]
      */
     protected $fillable = [
-        'name', 'email',
+        'name',
+        'email',
+        'password',
+        'role',
     ];
 
     /**
